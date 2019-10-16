23c807106cc5dde43b226ebbf3742a184f2e0785
qixuan zhang <qixuanzhang@qixuandeMacBook-Pro.local>
2019-10-17 00:20:51 +1100

    can show products

--- src/app/product-show/product-show.component.ts
+++ src/app/product-show/product-show.component.ts
@@ -9,6 +9,16 @@ import {ProductService} from '../product.service';
 })
 export class ProductShowComponent implements OnInit {
   products: Product[];
+  listproducts = [];
+  productsArray = {
+    productId: 0,
+    productName: '',
+    unitPrice: 0,
+    unit: '',
+    imgUrl: '',
+    categoryId: 0,
+    supplierId: 0
+  };
   error = '';
   success = '';
 
@@ -16,11 +26,16 @@ export class ProductShowComponent implements OnInit {
 
   ngOnInit() {
     this.getProducts();
+    console.log(this.productsArray);
   }
   getProducts(): void {
     this.productService.getAll().subscribe(
       (res: Product[]) => {
         this.products = res;
+        for(let i = 0; i < res.length; i++){
+          this.listproducts[i] = res[i];
+        }
+        console.log(this.productsArray[0]);
       },
       (err) => {
         this.error = err;
